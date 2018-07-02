69ecf07a67ee1e95d6cf200086b31d7171102700
meng8450 <348778450@qq.com>
2018-07-02 14:12:22 +0800

    实训栏目、轮播图：bug

--- src/components/Training.vue
+++ src/components/Training.vue
@@ -2,7 +2,12 @@
 <div id="Training">
     <!--轮播图-->
     <trainCarousel/>
-    
+    <!--实训栏目-->
+    <div class="theProject" v-for="(item,index) in theme" :key="index">
+        <img :src="item.image">
+        <h2>{{item.name}}</h2>
+        <h4>{{item.text}}</h4>
+    </div>
 </div>
 </template>
 <script>
@@ -12,7 +17,28 @@ export default {
     components:{TrainCarousel},
     data(){
         return{
-
+            theme:[
+                {
+                    image:/static/img/theme1.png,
+                    name:"精准能力评估+针对性专项练习",
+                    text:"要练就要练最有用的"
+                },
+                {
+                    image:/static/img/theme1.png,
+                    name:"互联网公司真题+模考+面经",
+                    text:"公司考什么？怎么考？一刷便知！"
+                },
+                {
+                    image:/static/img/theme1.png,
+                    name:"精准能力评估+针对性专项练习",
+                    text:"要练就要练最有用的"
+                },
+                {
+                    image:/static/img/theme1.png,
+                    name:"精准能力评估+针对性专项练习",
+                    text:"要练就要练最有用的"
+                },
+            ]
         }
     }
 }
